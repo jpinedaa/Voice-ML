15d7d183aa348bd266f3e585e70ff65af28ead0d
root <j_jorge_p@msn.com>
2019-02-15 19:58:28 +0000

    changes in LoadArray and LoadData

--- LoadData.py
+++ LoadData.py
@@ -4,8 +4,8 @@ import numpy as np
 def LoadData(file):
     data = np.genfromtxt(file, delimiter=",")
     #print("load data:", data.shape)
-    samples = int(data.shape[0]/201)
-    data = data.reshape(samples,201,300,1)
+    samples = int(data.shape[0]/513)
+    data = data.reshape(samples,513,300,1)
     #print("load data2:",data.shape)
     return data
 
