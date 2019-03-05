d4f1f9bea0af4b50b93c976df884ee08be8dd2b9
root <j_jorge_p@msn.com>
2019-03-05 02:30:10 +0000

    pass 2

--- MN_Model2.py
+++ MN_Model2.py
@@ -25,10 +25,10 @@ ap.add_argument('-g', '--gpus', type=int, default=1, help='# of GPUs to use for 
 args = vars(ap.parse_args())
 G = args["gpus"]
 
-NUM_EPOCHS = 100
+NUM_EPOCHS = 40
 INIT_LR = 1e-5
 lr_decay = 1
-training_batch_size = 32
+training_batch_size = 128
 # samples_per_checkpoint = 1000
 validation_split = 0.01
 data_percent = 1
