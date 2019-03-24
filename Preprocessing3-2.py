68f5b9ee76f8be58c0391b28927cf0146c77f593
jorge damian pineda alvarado <j_jorge_p@msn.com>
2019-03-24 14:33:09 +0800

    changes to preprocessing 3-2

--- Preprocessing3-2.py
+++ Preprocessing3-2.py
@@ -11,6 +11,9 @@ counter = 1
 filename = filename[:-4 - len(str(counter - 1))] + str(counter) + filename[-4:]
 filename2 = filename2[:-4 - len(str(counter - 1))] + str(counter) + filename2[-4:]
 
+pairs_data = np.memmap('pairs_data.array', dtype= np.float64, mode= 'w+', shape= (45000, 2, 100, 40, 3))
+pairs_labels = np.memmap('pairs_labels.array', dtype= np.float64, mode= 'w+', shape= (45000, ))
+
 print("[INFO] Loading first file... ")
 
 with open(filename2, 'r') as file:
@@ -25,7 +28,47 @@ counter = counter + 1
 print("[INFO] Finished loading first file")
 print("labels shape: " + str(labels.shape))
 
+flag = 0
+i = 0
+total_pairs = 0
 while 1:
+
+    while i < (labels.shape[0] - 1):
+        #    if total_pairs == 45000:
+        #        break
+        print("[INFO]Arranging pair#" + str(total_pairs))
+        if flag == 0:
+            # pairs_data_tmp = np.zeros((1, 2, 100, 40, 3))
+            # pairs_data_tmp[0] = [data[i], data[i+1]]
+            # pairs_data = np.concatenate((pairs_data,pairs_data_tmp))
+            pairs_data[total_pairs] = [data[i], data[i + 1]]
+            # pairs_labels_tmp = np.zeros((1,))
+            if labels[i] == labels[i + 1]:
+                pairs_labels[total_pairs] = 0
+            else:
+                pairs_labels[total_pairs] = 1
+            # pairs_labels = np.concatenate((pairs_labels, pairs_labels_tmp))
+            flag = 1
+            i = i + 2
+            total_pairs = total_pairs + 1
+        else:
+            random.seed()
+            index = random.randint(0, (labels.shape[0] - 1))
+            # pairs_data_tmp = np.zeros((1, 2, 100, 40, 3))
+            # pairs_data_tmp[0] = [data[i], data[index]]
+            # pairs_data = np.concatenate((pairs_data, pairs_data_tmp)
+            pairs_data[total_pairs] = [data[i], data[index]]
+            # pairs_labels_tmp = np.zeros((1,))
+            if labels[i] == labels[index]:
+                pairs_labels[total_pairs] = 0
+            else:
+                pairs_labels[total_pairs] = 1
+            # pairs_labels = np.concatenate((pairs_labels, pairs_labels_tmp))
+            flag = 0
+            i = i + 1
+            total_pairs = total_pairs + 1
+        print("[INFO]pairs_data shape: " + str(pairs_data.shape) + " pairs_labels shape: " + str(pairs_labels.shape))
+
     print("[INFO] Loading file " + str(counter) + " ...")
 
     filename = filename[:-4 - len(str(counter - 1))] + str(counter) + filename[-4:]
@@ -39,62 +82,13 @@ while 1:
         data_temp = LoadData(file)
     counter = counter + 1
 
-    labels = np.concatenate((labels, labels_temp))
-    data = np.concatenate((data, data_temp))
+    labels = labels_temp
+    data = data_temp
 
     print("[INFO] Finished loading file")
     print("labels shape: " + str(labels.shape))
+    print("data shape: " + str(data.shape))
 
-pairs_data = np.memmap('pairs_data.array', dtype= np.float64, mode= 'w+', shape= (45000, 2, 100, 40, 3))
-pairs_labels = np.memmap('pairs_labels.array', dtype= np.float64, mode= 'w+', shape= (45000, ))
-
-print("[INFO]Arrangig first pair... ")
-#pairs_data = np.zeros((1,2,100,40,3))
-pairs_data[0] = [data[0],data[1]]
-print("pairs data shape: " + str(pairs_data.shape))
-#pairs_labels = np.zeros((1,))
-if labels[0] == labels[1]:
-    pairs_labels[0] = 0
-else:
-    pairs_labels[0] = 1
-flag = 1
-i = 1
-total_pairs = 1
-while i < (labels.shape[0] - 1):
-#    if total_pairs == 45000:
-#        break
-    print("[INFO]Arranging pair#" + str(total_pairs))
-    if flag == 0:
-        #pairs_data_tmp = np.zeros((1, 2, 100, 40, 3))
-        #pairs_data_tmp[0] = [data[i], data[i+1]]
-        #pairs_data = np.concatenate((pairs_data,pairs_data_tmp))
-        pairs_data[total_pairs] = [data[i], data[i + 1]]
-        #pairs_labels_tmp = np.zeros((1,))
-        if labels[i] == labels[i+1]:
-            pairs_labels[total_pairs] = 0
-        else:
-            pairs_labels[total_pairs] = 1
-        #pairs_labels = np.concatenate((pairs_labels, pairs_labels_tmp))
-        flag = 1
-        i = i + 2
-        total_pairs = total_pairs + 1
-    else:
-        random.seed()
-        index = random.randint(0,(labels.shape[0]-1))
-        #pairs_data_tmp = np.zeros((1, 2, 100, 40, 3))
-        #pairs_data_tmp[0] = [data[i], data[index]]
-        #pairs_data = np.concatenate((pairs_data, pairs_data_tmp)
-        pairs_data[total_pairs] = [data[i], data[index]]
-        #pairs_labels_tmp = np.zeros((1,))
-        if labels[i] == labels[index]:
-            pairs_labels[total_pairs] = 0
-        else:
-            pairs_labels[total_pairs] = 1
-        #pairs_labels = np.concatenate((pairs_labels, pairs_labels_tmp))
-        flag = 0
-        i = i + 1
-        total_pairs = total_pairs + 1
-    print("[INFO]pairs_data shape: " + str(pairs_data.shape) + " pairs_labels shape: " + str(pairs_labels.shape))
 
 #pairs_data1 = np.memmap('pairs_data.array', dtype= np.float64, mode= 'w+', shape= pairs_data.shape)
 #pairs_labels1 = np.memmap('pairs_labels.array', dtype= np.float64, mode= 'w+', shape= pairs_labels.shape)
